33eaf42404640237af259e029afe1acaa9ab1e7f
unagipai <unagipai.unagipai@gmail.com>
2010-03-12 21:23:37 +0900

    kousin

--- xls2csvoutput.rb
+++ xls2csvoutput.rb
@@ -9,14 +9,14 @@ def getAbsolutePath filename
 end
 
 # ファイルに書き出し
-def xlsOutput(path)
+def xlsOutput(path, output_path)
   begin
     filename = getAbsolutePath(path)		#パス指定
     xl = WIN32OLE.new('Excel.Application')	#Excel起動
     book = xl.Workbooks.Open(filename)		#ExcelFileを開く
-    output_file = File.open("#{path}.csv", "w")	#ファイルがなければ新規作成、あればクリア
     
     book.Worksheets.each do |sheet|		#対象のブックのすべてのシートに処理
+      output_file = File.open("#{output_path}#{path}_#{sheet.name}.csv","w")
       sheet.UsedRange.Rows.each do |row|	#ワークシートの使用している範囲を一行ごとに取り出す
         record = []				#出力するために使用する配列初期化
         row.Columns.each do |cell|
@@ -34,19 +34,19 @@ def xlsOutput(path)
         puts record.join(",")
         output_file.write("#{record}\n")    # ファイルにデータ書き込み
       end
+      output_file.close #ファイルを閉じる
     end
   ensure
     book.Close		#Bookを閉じる
     xl.Quit		#Excel終了
-    output_file.close   #ファイルを閉じる
   end
 end
 
 # ディレクトリを処理する
-def proc_directory(path)
+def proc_directory(path, out_path)
   Find.find(path) do |file|
     if(File.file?(file) && File.extname(file) == '.xls' ) then
-      xlsOutput(file)
+      xlsOutput(file, out_path)
     end
   end
 end
@@ -58,17 +58,18 @@ def usage
   puts "  ruby #{__FILE__} [dirname]"
 end
 
-if ARGV.empty?
+if ARGV[0].empty?
   usage
   exit
 end
 
-target = ARGV[0]
+target = ARGV[0]	#処理対象Excelファイル用引数指定
+output_path = ARGV[1]	#保存先指定用引数指定
 
 if(File.file?(target)) then # ファイルが渡された
-  xlsOutput(target)
+  xlsOutput(target, output_path)
 elsif(File.directory?(target)) then # ディレクトリが渡された
-  proc_directory(target)
+  #proc_directory(target, output_path)
 else
   usage
   exit
